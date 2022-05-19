4841effd931e480619e64f52297ba7e3a9990e7d
awesysosexy <Danielsbhansen@hotmail.com>
2022-05-19 13:47:30 +0200

    dhcp

--- Opsætning af ADDS.ps1
+++ Opsætning af ADDS.ps1
@@ -26,4 +26,24 @@ for($i=0; $i -le $amountOfUsers; $i++){
     New-ADUser -Name $tmp_navn -Accountpassword (Read-Host -AsSecureString "Password for $tmp_navn")  -path "OU=BrugerAccounts, DC=$DomainName, DC=com" -Enabled $true
     Add-ADGroupMember -Identity $tmp_department -Members $tmp_navn
 }
-#123
+
+#Install DHCP
+Install-WindowsFeature DHCP -IncludeManagementTools
+
+#Create DHCP security group
+netsh dhcp add securitygroups
+
+#Restart DHCP Service
+Restart-Service dhcpserver
+
+#Authorize the DHCP-Server
+Add-DhcpServerInDC -DnsName DC1.WindowsSux.com -IPAddress 192.168.1.2
+
+#Enable Dynamic DNS updates
+Set-DhcpServerv4DnsSetting -ComputerName "DC1.WindowsSux.com" -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
+
+#Configure the scope
+Add-DhcpServerv4Scope -name "WindowsSux" -StartRange 192.168.1.11 -EndRange 192.168.1.254 -SubnetMask 255.255.255.0 -State Active
+Add-DhcpServerv4ExclusionRange -ScopeID 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.10
+Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.1.2 -ScopeID 192.168.1.0 -ComputerName DC1.WindowsSux.com
+Set-DhcpServerv4OptionValue -DnsDomain WindowsSux.com -DnsServer 192.168.1.2
