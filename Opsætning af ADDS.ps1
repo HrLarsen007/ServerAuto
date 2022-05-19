072fdd4c3ce1aa9b4dffe7996a5ef885f12bc792
Morten Larsen <hrlarsen007@gmail.com>
2022-05-19 15:23:21 +0200

    Overhaul

--- Opsætning af ADDS.ps1
+++ Opsætning af ADDS.ps1
@@ -1,49 +1,90 @@
-Add-WindowsFeature AD-Domain-Services
-## et module til at gøre vores AD mere smooth
-Import-Module ADDSDeployment
+# Setting Variables that would be needed to setup the services
 
-$DomainName = Read-Host "Domain Navn"
+$DomainName = Read-Host "Domain Navn (Example: Spaceballs)"
+$DomainEnd = Read-Host "Domain end  (Example: com"
+$DomainPcName = Read-Host "PC Name (Example: DC1)"
 
-Install-ADDSForest -DomainName $DomainName.com -InstallDNS -NoRebootOnCompletion
+# Install ADDS Service
+$ConfirmPreference="high"
+function Install_ADDS {
+    [CmdletBinding(
+    SupportsShouldProcess = $true,
+    ConfirmImpact = 'High')]
+    param($param)
+    if ($PSCmdlet.ShouldProcess($param)) {
+        Add-WindowsFeature AD-Domain-Services
+        Import-Module ADDSDeployment
 
-New-ADOrganizationalUnit -Name "BrugerAccounts" -Path "DC=$DomainName,DC=COM"
+        Install-ADDSForest -DomainName "$($DomainName).$($DomainEnd)" -InstallDNS -NoRebootOnCompletion
 
-$amountOfGroups = Read-HosT "Hvor mange groups vil du lave?"
-for($i=0; $i -le $amountOfGroups; $i++){
-    $tmp_group = Read-Host "Group Navn"
-    New-ADGroup -Name $tmp_group -GroupScope Global
-    New-ADGroup -Name $tmp_group_Share_File1_RA -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Read Access" 
-    New-ADGroup -Name $tmp_group_Share_File1_CA -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Change Access" 
-    New-ADGroup -Name $tmp_group_Share_File1_FA -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Full Access"
-    Add-ADGroupMember -Identity $tmp_group -Members $tmp_group_Share_File1_RA,$tmp_group_Share_File1_CA,$tmp_group_Share_File1_FA
+        New-ADOrganizationalUnit -Name "BrugerAccounts" -Path "DC="$($DomainName)",DC="$($DomainEnd)""
+        $amountOfGroups = Read-HosT "Hvor mange groups vil du lave?"
+        for($i=0; $i -le $amountOfGroups; $i++){
+            $tmp_group = Read-Host "Group Navn"
+            New-ADGroup -Name $tmp_group -GroupScope DomainLocal
+            New-ADGroup -Name "$($tmp_group)_Share_File1_RA" -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Read Access" 
+            New-ADGroup -Name "$($tmp_group)_Share_File1_CA" -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Change Access" 
+            New-ADGroup -Name "$($tmp_group)_Share_File1_FA" -GroupScope DomainLocal -GroupCategory Security -Description "$tmp_group Full Access"
+            Add-ADGroupMember -Identity $tmp_group -Members "$($tmp_group)_Share_File1_RA","$($tmp_group)_Share_File1_CA","$($tmp_group)_Share_File1_FA"
+        }
+
+        $amountOfUsers = Read-Host "Hvor mange brugere vil du lave?"
+        for($i=0; $i -le $amountOfUsers; $i++){
+            $tmp_navn = Read-Host "Navn"
+            $tmp_department = Read-Host "Afdeling"
+
+            New-ADUser -Name "$tmp_navn" -Accountpassword (Read-Host -AsSecureString "Password")  -Path "OU=BrugerAccounts, DC="$($DomainName)", DC="$($DomainEnd)"" -Enabled $true
+            Add-ADGroupMember -Identity "$tmp_department" -Members "$tmp_navn"
+        }
+    }
 }
 
-$amountOfUsers = Read-Host "Hvor mange brugere vil du lave?"
-for($i=0; $i -le $amountOfUsers; $i++){
-    $tmp_navn = Read-Host "Navn"
-    $tmp_department = Read-Host "Afdeling"
+function Install_DHCP {
+    [CmdletBinding(
+    SupportsShouldProcess = $true,
+    ConfirmImpact = 'High')]
+    param($param)
+    if ($PSCmdlet.ShouldProcess($param)) {
+        #Install DHCP
+        Install-WindowsFeature DHCP -IncludeManagementTools
 
-    New-ADUser -Name $tmp_navn -Accountpassword (Read-Host -AsSecureString "Password for $tmp_navn")  -path "OU=BrugerAccounts, DC=$DomainName, DC=com" -Enabled $true
-    Add-ADGroupMember -Identity $tmp_department -Members $tmp_navn
+        #Create DHCP security group
+        netsh dhcp add securitygroups
+
+        #Restart DHCP Service
+        Restart-Service dhcpserver
+
+        #Authorize the DHCP-Server
+        Add-DhcpServerInDC -DnsName "$($DomainPcName).$($DomainName).$($DomainEnd)" -IPAddress 192.168.1.2
+
+        #Enable Dynamic DNS updates
+        Set-DhcpServerv4DnsSetting -ComputerName "$($DomainPcName).$($DomainName).$($DomainEnd)" -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
+
+        #Configure the scope
+        Add-DhcpServerv4Scope -name "$DomainName" -StartRange 192.168.1.11 -EndRange 192.168.1.254 -SubnetMask 255.255.255.0 -State Active
+        Add-DhcpServerv4ExclusionRange -ScopeID 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.10
+        Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.1.1 -ScopeID 192.168.1.0
+        Set-DhcpServerv4OptionValue -DnsDomain "$($DomainName).$($DomainEnd)" -DnsServer 192.168.1.2
+    }
 }
 
-#Install DHCP
-Install-WindowsFeature DHCP -IncludeManagementTools
+function Install_SHARE {
+    [CmdletBinding(
+    SupportsShouldProcess = $true,
+    ConfirmImpact = 'High')]
+    param($param)
+    if ($PSCmdlet.ShouldProcess($param)) {
 
-#Create DHCP security group
-netsh dhcp add securitygroups
+        Install-WindowsFeature File-Services
+        md C:\File1\Salg
+        md C:\File1\HR
+        md C:\File1\IT
+        New-SmbShare -Name IT -Path C:\File1\Salg -EncryptData $True -FullAccess IT_Share_File1_FA -ReadAccess HR_Share_File1_RA, Salg_Share_File1_RA
+        New-SmbShare -Name HR -Path C:\File1\HR -EncryptData $True -FullAccess IT_Share_File1_FA -ChangeAccess HR_Share_File1_CA
+        New-SmbShare -name Salg -Path C:\File1\Salg -EncryptData $True -FullAccess IT_Share_File1_FA -ChangeAccess Salg_Share_File1_CA
+    }
+}
 
-#Restart DHCP Service
-Restart-Service dhcpserver
-
-#Authorize the DHCP-Server
-Add-DhcpServerInDC -DnsName DC1.WindowsSux.com -IPAddress 192.168.1.2
-
-#Enable Dynamic DNS updates
-Set-DhcpServerv4DnsSetting -ComputerName "DC1.WindowsSux.com" -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
-
-#Configure the scope
-Add-DhcpServerv4Scope -name "WindowsSux" -StartRange 192.168.1.11 -EndRange 192.168.1.254 -SubnetMask 255.255.255.0 -State Active
-Add-DhcpServerv4ExclusionRange -ScopeID 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.10
-Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.1.1 -ScopeID 192.168.1.0 -ComputerName pfsense.WindowsSux.com
-Set-DhcpServerv4OptionValue -DnsDomain WindowsSux.com -DnsServer 192.168.1.2
+Install_ADDS
+Install_DHCP
+Install_SHARE
